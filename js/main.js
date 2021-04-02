2d4ac76e5dedd61307f5554903e636a5af351907
Eric Davis <edavis211@mac.com>
2021-04-01 23:08:35 -0400

    about page v1

--- js/main.js
+++ js/main.js
@@ -1,38 +1,5 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-  console.log("DOM fully loaded and parsed");
-
-  var homeEventSlider = $(".owl-carousel").owlCarousel({
-    loop: true,
-    dotsEach: 1,
-    items: 1,
-  });
-
   var rellax = new Rellax(".rellax");
-
-  /*
-  document.getElementById("scrolldown").addEventListener("click", function () {
-    var offset = 80; // sticky nav height
-    var main = document.getElementById("front-page-main");
-    window.scroll({
-      top: main.offsetTop - offset,
-      left: 0,
-      behavior: "smooth",
-    });
-  });
-  */
-
-  document.querySelectorAll(".filter").forEach(function (filter) {
-    filter.addEventListener("click", function () {
-      filter.classList.toggle("active");
-    });
-  });
-
-  $("#slide-prev").on("click", function () {
-    homeEventSlider.trigger("prev.owl.carousel");
-  });
-  $("#slide-next").on("click", function () {
-    homeEventSlider.trigger("next.owl.carousel");
-  });
 
   $("#search-button").on("click", function () {
     $("#navsearch").toggleClass("active");
