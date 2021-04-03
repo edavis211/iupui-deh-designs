bc77a75fcaeaa8cd1513f0a27220f701af5f0f20
Eric Davis <edavis211@mac.com>
2021-04-03 11:16:47 -0400

    about v2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-  var rellax = new Rellax(".rellax");
-
   $("#search-button").on("click", function () {
     $("#navsearch").toggleClass("active");
   });
